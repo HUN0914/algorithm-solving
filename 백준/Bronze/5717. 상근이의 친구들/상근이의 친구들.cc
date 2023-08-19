4c920c3144ebcd83db96e6b1a184f458a1f96943
HUN0914 <74861510+HUN0914@users.noreply.github.com>
2023-08-19 22:27:14 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n,m;
+    int sum;
+    while(1)
+    {
+        cin>>n>>m;
+        
+        if(n==0&&m==0)
+        {
+            break;
+        }
+        cout<<n+m<<endl;
+        
+    }
+    
+}
