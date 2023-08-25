7166ded039bc68d76bc8cd0642fbe9ecf005dbc1
HUN0914 <74861510+HUN0914@users.noreply.github.com>
2023-08-25 11:14:17 +0900

    [Silver III] Title: 이전 순열, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> v;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+
+    if (prev_permutation(v.begin(), v.end())) {
+        for (int i = 0; i < n; i++) {
+            cout << v[i] << " ";
+        }
+    }
+    else {
+   /*     for (int i = 0; i < n; i++) {
+            cout << v[i] << " ";
+        }*/
+        cout << -1 << "\n";
+    }
+
+}
