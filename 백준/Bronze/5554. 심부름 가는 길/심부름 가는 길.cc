bbd1790bc3cb46afcc815e8703980a6b83a46b18
HUN0914 <74861510+HUN0914@users.noreply.github.com>
2023-08-31 18:38:12 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int x1,x2,x3,x4;
+    int sum=0;
+    cin>>x1>>x2>>x3>>x4;
+    
+    sum=x1+x2+x3+x4;
+    
+    cout<<sum/60<<endl<<sum%60;
+
+
+
+    return 0;
+}
