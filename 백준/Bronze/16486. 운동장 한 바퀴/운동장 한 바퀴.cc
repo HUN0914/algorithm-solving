fa7d9107b5daf5b99d447045e2b57f297466bf08
HUN0914 <74861510+HUN0914@users.noreply.github.com>
2023-09-04 20:24:53 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> v;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    double n;
+    double m;
+    double result;
+
+    cin >> n >> m;
+
+   // cout << (3.141592 * 2*m) + 2 * n;
+
+    printf("%lf", (3.141592 * 2 * m) + 2 * n);
+
+    return 0;
+
+
+}
