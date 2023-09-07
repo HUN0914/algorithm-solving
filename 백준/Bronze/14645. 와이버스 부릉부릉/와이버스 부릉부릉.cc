6c34fefb5917f7f1e0409072a1092c9a77f1542c
HUN0914 <74861510+HUN0914@users.noreply.github.com>
2023-09-07 09:37:59 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(void)
+{
+    int n,m;
+    int val1,val2;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            cin>>val1>>val2;
+        }
+    }
+    
+    cout<<"비와이";
+    
+    
+}
