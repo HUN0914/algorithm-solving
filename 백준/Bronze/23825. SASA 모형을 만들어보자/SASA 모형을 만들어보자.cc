c537afdac94bd868865ff0e4d7b0992f876727bd
HUN0914 <74861510+HUN0914@users.noreply.github.com>
2023-09-11 23:41:42 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+    int n,m;
+    cin>>n>>m;
+    
+    int val=min(n/2,m/2);
+    
+    cout<<val;
+    
+    
+    return 0;
+}
