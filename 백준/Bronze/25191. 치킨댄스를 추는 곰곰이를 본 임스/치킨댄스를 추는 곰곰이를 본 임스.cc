8442f07d3ae62824da1b1c55c2bef11d4c74498f
HUN0914 <74861510+HUN0914@users.noreply.github.com>
2023-10-11 20:01:41 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25191. 치킨댄스를 추는 곰곰이를 본 임스/치킨댄스를 추는 곰곰이를 본 임스.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n,a,b;
+    
+    cin>>n>>a>>b;
+    
+    if(n<=((a/2)+b))
+    {
+        cout<<n;
+    }
+    else
+    {
+        cout<<a/2+b;
+    }
+
+
+    return 0;
+}
