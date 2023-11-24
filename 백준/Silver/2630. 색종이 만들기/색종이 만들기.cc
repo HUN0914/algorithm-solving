4ab57421f9e3c72c65a6c2a3b2fb473c05e24685
HUN0914 <74861510+HUN0914@users.noreply.github.com>
2023-11-24 22:09:55 +0900

    [Silver II] Title: 색종이 만들기, Time: 4 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+int paper[128][128];
+int white;
+int blue;
+
+void solve(int y, int x, int size)
+{
+	int check = paper[y][x];
+
+	for(int i=y; i<y+size; i++)
+	{
+
+		for (int j = x; j < x+size; j++)
+		{
+			if (check != paper[i][j])
+			{
+				check = 2;
+			}
+			
+		}
+	}
+
+	if (check == 2)
+	{
+		solve(y, x, size / 2);
+		solve(y, x + size / 2, size / 2);
+		solve(y + size / 2, x, size / 2);
+		solve(y + size / 2, x + size / 2, size / 2);
+		return;
+
+	}
+
+	if (check == 0)
+	{
+		white++;
+	}
+	else if (check == 1)
+	{
+		blue++;
+	}
+}
+
+int main(void)
+{
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> paper[i][j];
+		}
+	}
+
+	solve(0, 0, n);
+
+	cout << white << "\n";
+	cout << blue << "\n";
+
+	return 0;
+}
