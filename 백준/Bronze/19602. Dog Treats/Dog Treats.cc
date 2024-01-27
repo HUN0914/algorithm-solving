8e06f9d713afe32f59e21059339a0c047aca73d6
HUN0914 <74861510+HUN0914@users.noreply.github.com>
2024-01-27 22:26:28 +0900

    [Bronze IV] Title: Dog Treats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int one, two, three;
+
+	cin >> one >> two >> three;
+
+	if (one * 1 + two * 2 + three * 3 >= 10)
+	{
+		cout << "happy";
+	}
+	else
+	{
+		cout<<"sad";
+	}
+
+	return 0;
+}
