f37d7ee1e7c04bb9315d87c967d409712e1e5704
HUN0914 <74861510+HUN0914@users.noreply.github.com>
2024-08-13 14:56:00 +0900

    [Silver I] Title: 카잉 달력, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+	if (b == 0) {
+		return a;
+	}
+	return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+	return (a * b) / gcd(a, b);
+}
+
+
+int main(void) {
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int m, n, x, y;
+		int result = -1;
+		cin >> m >> n >> x >> y;
+
+		int maxi = lcm(m, n);
+		for (int i = x; i <= maxi; i+=m) {
+			int ny = i % n;
+
+			if (ny == 0) {
+				ny = n;
+			}
+
+			if (ny == y) {
+				result = i;
+				break;
+			}
+
+		}
+		cout << result<<"\n";
+
+	}
+	
+
+	return 0;
+}
