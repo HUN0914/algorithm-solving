77ea78c8fd7f4d273037a560225d82d68eb96c0d
GWANGHUN BAEK <74861510+HUN0914@users.noreply.github.com>
2025-09-11 21:22:09 +0900

    [Silver II] Title: 수 이어 쓰기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string number;
+    cin>>number;
+    long long start=1;
+    long long i=0;
+
+    while (i<number.size()) {
+        string startString=to_string(start);
+
+            for (int j=0; j<startString.size(); j++){
+                if (startString[j]==number[i]) {
+                    i++;
+                    if (i==number.size()) break;
+                }
+            }
+            start++;
+    }
+    cout<<start-1;
+    return 0;
+}
