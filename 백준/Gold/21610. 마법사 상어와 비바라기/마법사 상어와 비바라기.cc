9d79e1f7c108bcd77fb3a09d9c6eb719d200c679
Gwanghun Baek <74861510+HUN0914@users.noreply.github.com>
2026-01-15 10:58:57 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 4 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.cc
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.cc
@@ -2,108 +2,98 @@
 
 using namespace std;
 
-int map[51][51];
+int currMap[51][51];
 bool isCloud[51][51];
-bool newCloud[51][51];
-int dy[9] = {0, 0, -1, -1, -1, 0, 1, 1, 1};
-int dx[9] = {0, -1, -1, 0, 1, 1, 1, 0, -1};
+bool isPrevCloud[51][51];
 int n,m;
+int dx[8]={-1,-1,0,1,1,1,0,-1};
+int dy[8]={0,-1,-1,-1,0,1,1,1};
+
+int ax[4]={-1,-1,1,1};
+int ay[4]={-1,1,-1,1};
 
 void move(int d, int s) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    
+    int dir=d-1;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            isPrevCloud[i][j]=false;
+        }
+    }
+
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
             if (isCloud[i][j]) {
-                int ny = (i + dy[d] * s + n) % n;
-                int nx = (j + dx[d] * s + n) % n;
-                newCloud[ny][nx] = true;
+                s=s%n;
+                int py=(i+s*dy[dir]+n)%n;
+                int px=(j+s*dx[dir]+n)%n;
+                isPrevCloud[py][px]=true;
+                isCloud[i][j]=false;
             }
         }
     }
 }
 
 void rain() {
+
     for (int i=0; i<n; i++) {
         for (int j=0; j<n; j++) {
-            if (newCloud[i][j]) {
-                map[i][j]=map[i][j]+1;
+            if (isPrevCloud[i][j]) {
+                currMap[i][j]++;
             }
         }
     }
 }
 
-void waterCopyBugAndNoCloud() {
+void waterCopy() {
 
     for (int i=0; i<n; i++) {
         for (int j=0; j<n; j++) {
-            if (newCloud[i][j]) {
-                if (i-1>=0&&j-1>=0&&map[i-1][j-1]>0) map[i][j]++;
-                if (i-1>=0&&j+1<n&&map[i-1][j+1]>0) map[i][j]++;
-                if (i+1<n&&j-1>=0&&map[i+1][j-1]>0) map[i][j]++;
-                if (i+1<n&&j+1<n&&map[i+1][j+1]>0) map[i][j]++;
+            if (isPrevCloud[i][j]) {
+                int waterCount=0;
+                for (int k=0; k<4; k++) {
+                    int ny=i+ay[k];
+                    int nx=j+ax[k];
+                    if (ny>=0&&ny<n&&nx>=0&&nx<n) {
+                        if (currMap[ny][nx]>0) waterCount++;
+                    }
+                }
+                currMap[i][j]+=waterCount;
             }
         }
     }
+
+
 }
 
-void newCloudCreate() {
+void createCloud() {
 
     for (int i=0; i<n; i++) {
         for (int j=0; j<n; j++) {
-            isCloud[i][j]=false;
-        }
-    }
-
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
-            if (!newCloud[i][j]&&map[i][j]>=2) {
-                map[i][j]=map[i][j]-2;
+            if (!isPrevCloud[i][j]&&currMap[i][j]>=2) {
                 isCloud[i][j]=true;
+                currMap[i][j]=currMap[i][j]-2;
             }
-        }
-    }
-
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
-            newCloud[i][j]=false;
         }
     }
 }
 
 void game(int d, int s) {
-    s%=n;
-    for (int i=0;i<n;i++)
-        for (int j=0;j<n;j++)
-            newCloud[i][j]=false;
-
+    
     move(d,s);
     rain();
-    waterCopyBugAndNoCloud();
-    newCloudCreate();
-}
-
-int sumResult() {
-    int value=0;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
-            value+=map[i][j];
-        }
-    }
-    return value;
+    waterCopy();
+    createCloud();
 }
 
 int main() {
 
     cin>>n>>m;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
-            cin>>map[i][j];
-        }
-    }
+    for (int i=0; i<n; i++)
+        for (int j=0; j<n; j++) cin>>currMap[i][j];
 
-    isCloud[n-2][0]=true;
-    isCloud[n-2][1]=true;
-    isCloud[n-1][0]=true;
-    isCloud[n-1][1]=true;
+    for (int i=n-2; i<n; i++)
+        for (int j=0; j<2; j++) isCloud[i][j]=true;
 
     for (int i=0; i<m; i++) {
         int d,s;
@@ -111,8 +101,11 @@ int main() {
         game(d,s);
     }
 
-    int result=sumResult();
+    int result=0;
+    for (int i=0; i<n; i++)
+        for (int j=0; j<n; j++) result+=currMap[i][j];
+    
     cout<<result;
 
-    return 0;
+        return 0;
 }
