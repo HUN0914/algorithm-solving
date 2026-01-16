f573ca5adf300e3125a401979d3093943bbc930b
Gwanghun Baek <74861510+HUN0914@users.noreply.github.com>
2026-01-16 16:17:37 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 108 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -2,46 +2,84 @@
 
 using namespace std;
 
-int map[17][17];
 int dy[3]={0,1,1};
-int dx[3]={1,0,1};
+int dx[3]={1,1,0};
+int board[17][17];
 int n;
-int answer=0;
+int answer;
 
 void dfs(int y, int x, int dir) {
 
-    if (y==n-1&&x==n-1) {
+    if (x==n-1&&y==n-1) {
         answer++;
         return;
     }
 
-    for (int i=0; i<3; i++) {
-        int ny=y+dy[i];
-        int nx=x+dx[i];
+    if (dir==0) {
+        for (int i=0; i<2; i++) {
+            int ny=y+dy[i];
+            int nx=x+dx[i];
+            if (ny<0||ny>=n||nx<0||nx>=n) continue;
+            if (board[ny][nx]==1) continue;
+            if (i==0) {
+                dfs(ny,nx,i);
+            }else {
+                if (board[y+1][x]!=1&&board[y][x+1]!=1) {
+                    dfs(ny,nx,i);
+                }
+            }
+        }
+    }else if (dir==1) {
+        for (int i=0; i<3; i++) {
+            int ny=y+dy[i];
+            int nx=x+dx[i];
+            if (ny<0||ny>=n||nx<0||nx>=n) continue;
+            if (board[ny][nx]==1) continue;
 
-        if (ny>=n||ny<0||nx>=n||nx<0) continue;
-        if (map[ny][nx]==1) continue;
-        if (i==0 && dir==1) continue;
-        if (i==1 && dir==0) continue;
-        if (i==2&& (map[y+1][x]==1||map[y][x+1]==1)) continue;
-        dfs(ny,nx,i);
-
+            if (i==1) {
+                if (board[y+1][x]!=1&&board[y][x+1]!=1) {
+                    dfs(ny,nx,i);
+                }
+            }else {
+                dfs(ny,nx,i);
+            }
+        }
+    }else if (dir==2) {
+        for (int i=1; i<3; i++) {
+            int ny=y+dy[i];
+            int nx=x+dx[i];
+            if (ny<0||ny>=n||nx<0||nx>=n) continue;
+            if (board[ny][nx]==1) continue;
+            if (i==1) {
+                if (board[y+1][x]!=1&&board[y][x+1]!=1) {
+                    dfs(ny,nx,i);
+                }
+            }else {
+                dfs(ny,nx,i);
+            }
+        }
     }
 
 }
 
-int main() {
+void input() {
 
     cin>>n;
     for (int i=0; i<n; i++) {
         for (int j=0; j<n; j++) {
-            cin>>map[i][j];
+            cin>>board[i][j];
         }
     }
 
+}
+
+void game() {
+    input();
     dfs(0,1,0);
-
     cout<<answer;
+}
 
+int main() {
+    game();
     return 0;
 }
