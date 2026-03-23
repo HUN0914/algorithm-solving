4455a196a472677e1d3500f0668466350203671a
Gwanghun Baek <74861510+HUN0914@users.noreply.github.com>
2026-03-23 11:08:17 +0900

    [Gold III] Title: 감시, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,139 +1,208 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int maping[8][8];
-vector<pair<int, int>>v;
-int n, m;
-int dx[4] = { 0, -1, 0, 1 };
-int dy[4] = { 1, 0, -1, 0 };
-int mini = 987654321;
+int dy[4]={0,-1,0,1};
+int dx[4]={1,0,-1,0};
+int n,m;
 
-void initial() {
+struct cctv {
+    int y;
+    int x;
+    char number;
+};
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-		{
-			cin >> maping[i][j];
-			
-			if (maping[i][j] != 0 && maping[i][j] != 6)
-			{
-				v.push_back({ i,j });
-			}
-		}
-	}
+vector<cctv>cctvs;
 
+char board[8][8];
+
+int maximum=1e9;
+
+void dfs(int cnt) {
+
+    if (cnt==cctvs.size()) {
+        int curTotal=0;
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (board[i][j]=='0') curTotal++;
+            }
+        }
+        maximum=min(maximum,curTotal);
+        return;
+    }
+
+    for (int i=0; i<4; i++){
+        int y=cctvs[cnt].y;
+        int x=cctvs[cnt].x;
+        char number=cctvs[cnt].number;
+        char copyBoard[8][8];
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                copyBoard[i][j]=board[i][j];
+            }
+        }
+
+        if (number=='1') {
+            while (true) {
+                y+=dy[i];
+                x+=dx[i];
+                if (y<0||y>=n||x<0||x>=m) break;
+                if (board[y][x]=='6') break;
+                if (board[y][x]=='0') board[y][x]='#';
+            }
+        }
+        else if (number=='2') {
+            int newPy=y;
+            int newPx=x;
+
+            //오른쪽, 위, 왼쪽, 아래 (0,1,2,3) -> [0,2] [1,3]
+            while (true) {
+                newPy+=dy[i];
+                newPx+=dx[i];
+                if (newPy<0||newPy>=n||newPx<0||newPx>=m) break;
+                if (board[newPy][newPx]=='6') break;
+                if (board[newPy][newPx] =='0') board[newPy][newPx]='#';
+            }
+
+            newPy=y;
+            newPx=x;
+            while (true) {
+                newPy+=dy[(i+2)%4];
+                newPx+=dx[(i+2)%4];
+                if (newPy<0||newPy>=n||newPx<0||newPx>=m) break;
+                if (board[newPy][newPx]=='6') break;
+                if (board[newPy][newPx] =='0') board[newPy][newPx]='#';
+            }
+        }
+        else if (number=='3') {
+            int newPy=y;
+            int newPx=x;
+
+            //오른쪽, 위, 왼쪽, 아래 (0,1,2,3) -> [0,2] [1,3]
+            while (true) {
+                newPy+=dy[i];
+                newPx+=dx[i];
+                if (newPy<0||newPy>=n||newPx<0||newPx>=m) break;
+                if (board[newPy][newPx]=='6') break;
+                if (board[newPy][newPx] =='0') board[newPy][newPx]='#';
+            }
+
+            newPy=y;
+            newPx=x;
+            while (true) {
+                newPy+=dy[(i+1)%4];
+                newPx+=dx[(i+1)%4];
+                if (newPy<0||newPy>=n||newPx<0||newPx>=m) break;
+                if (board[newPy][newPx]=='6') break;
+                if (board[newPy][newPx] =='0') board[newPy][newPx]='#';
+            }
+        }
+        else if (number=='4') {
+            int newPy=y;
+            int newPx=x;
+
+            //오른쪽, 위, 왼쪽, 아래 (0,1,2,3) -> [0,2] [1,3]
+            while (true) {
+                newPy+=dy[i];
+                newPx+=dx[i];
+                if (newPy<0||newPy>=n||newPx<0||newPx>=m) break;
+                if (board[newPy][newPx]=='6') break;
+                if (board[newPy][newPx] =='0') board[newPy][newPx]='#';
+            }
+
+            newPy=y;
+            newPx=x;
+            while (true) {
+                newPy+=dy[(i+1)%4];
+                newPx+=dx[(i+1)%4];
+                if (newPy<0||newPy>=n||newPx<0||newPx>=m) break;
+                if (board[newPy][newPx]=='6') break;
+                if (board[newPy][newPx] =='0') board[newPy][newPx]='#';
+            }
+
+            newPy=y;
+            newPx=x;
+            while (true) {
+                newPy+=dy[(i+2)%4];
+                newPx+=dx[(i+2)%4];
+                if (newPy<0||newPy>=n||newPx<0||newPx>=m) break;
+                if (board[newPy][newPx]=='6') break;
+                if (board[newPy][newPx] =='0') board[newPy][newPx]='#';
+            }
+
+        }
+        else if (number=='5') {
+            int newPy=y;
+            int newPx=x;
+
+            //오른쪽, 위, 왼쪽, 아래 (0,1,2,3) -> [0,2] [1,3]
+            while (true) {
+                newPy+=dy[i];
+                newPx+=dx[i];
+                if (newPy<0||newPy>=n||newPx<0||newPx>=m) break;
+                if (board[newPy][newPx]=='6') break;
+                if (board[newPy][newPx] =='0') board[newPy][newPx]='#';
+            }
+
+            newPy=y;
+            newPx=x;
+            while (true) {
+                newPy+=dy[(i+1)%4];
+                newPx+=dx[(i+1)%4];
+                if (newPy<0||newPy>=n||newPx<0||newPx>=m) break;
+                if (board[newPy][newPx]=='6') break;
+                if (board[newPy][newPx] =='0') board[newPy][newPx]='#';
+            }
+
+            newPy=y;
+            newPx=x;
+            while (true) {
+                newPy+=dy[(i+2)%4];
+                newPx+=dx[(i+2)%4];
+                if (newPy<0||newPy>=n||newPx<0||newPx>=m) break;
+                if (board[newPy][newPx]=='6') break;
+                if (board[newPy][newPx] =='0') board[newPy][newPx]='#';
+            }
+
+            newPy=y;
+            newPx=x;
+            while (true) {
+                newPy+=dy[(i+3)%4];
+                newPx+=dx[(i+3)%4];
+                if (newPy<0||newPy>=n||newPx<0||newPx>=m) break;
+                if (board[newPy][newPx]=='6') break;
+                if (board[newPy][newPx] =='0') board[newPy][newPx]='#';
+            }
+        }
+        dfs(cnt+1);
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                board[i][j]=copyBoard[i][j];
+            }
+        }
+    }
 }
 
-void check(int y, int x, int dir)
-{
-	dir %= 4;
+void input() {
+    cin>>n>>m;
 
-	while (1) {
-		int nx = x + dx[dir];
-		int ny = y + dy[dir];
-
-		x = nx;
-		y = ny;
-		/*
-		
-		!! 반복문이다 !!
-		x=nx, y=ny로 갱신 안해주면 무한 루프에 빠질 수도 있음.
-		우리의 목적은 ***각 위치****에서 어떤 카메라이냐에 따라서
-		하나하나씩 위치를 봐주는 것이기 때문에 위와 같이 해줘야 한다.
-		
-		*/
-
-		if (nx < 0 || nx >= m || ny < 0 || ny >= n) return;
-		if (maping[ny][nx] == 6) return;
-		if (maping[ny][nx] != 0) continue;
-		maping[ny][nx] = -1;
-		
-	}
-
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cin>>board[i][j];
+            if (board[i][j]!='6'&&board[i][j]!='0') {
+                cctvs.push_back({i,j,board[i][j]});
+            }
+        }
+    }
 }
 
-void dfs(int cnt)
-{
-	if (cnt == v.size())
-	{
-		int result = 0;
+int main() {
 
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				if (!maping[i][j]) result++;
-			}
-		}
-		mini = min(mini, result);
-		return;
-	}
+    input();
+    dfs(0);
+    cout<<maximum;
 
-	int x = v[cnt].first;
-	int y = v[cnt].second;
-	int tmp[9][9];
-
-	for (int dir = 0; dir < 4; dir++) {
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < m; j++)
-				tmp[i][j] = maping[i][j]; 
-		/*
-		여러 케이스를 봐야하므로, 
-		각 초기 배열값을 재귀 시작전에 늘 갖고 감. 즉, 재귀로 돌아올 때마다의
-		그떄 사용한 case는 없던 취급을 해줘야 하기 때문에, 반복문 안에서 담아준 것이다.
-		*/
-		
-		//여기서 하는 dir은 dx,dy 배열에 담긴 값에 따른 위치 구분
-		if (maping[x][y] == 1)
-		{
-			check(x, y, dir);
-		}
-		else if (maping[x][y] == 2)
-		{
-			check(x, y, dir);
-			check(x, y, dir + 2);
-		}
-		else if (maping[x][y] == 3)
-		{
-			check(x, y, dir);
-			check(x, y, dir + 1);
-		}
-		else if (maping[x][y] == 4) {
-			check(x, y, dir);
-			check(x, y, dir + 1);
-			check(x, y, dir + 2);
-		}
-		else if (maping[x][y] == 5) {
-			check(x, y, dir);
-			check(x, y, dir + 1);
-			check(x, y, dir + 2);
-			check(x, y, dir + 3);
-		}
-
-		dfs(cnt + 1);
-
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < m; j++)
-				maping[i][j] = tmp[i][j];
-		/*
-		손상된 배열을 돌려주는 것.
-		*/
-	}
-
-}
-
-int main(void)
-{
-	cin >> n >> m;
-	
-	initial();
-	dfs(0);
-
-	cout << mini << '\n';
-
-	return 0;
+    return 0;
 }
