997eb8990285a77db3bfc3e9475a4036a35b161c
Gwanghun Baek <74861510+HUN0914@users.noreply.github.com>
2026-05-05 18:51:12 +0900

    [level 3] Title: 거스름돈, Time: 4.46 ms, Memory: 3.98 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std; 
+
+int dp[100001];
+
+int dynamic(int n, vector<int> money){
+    dp[0]=1;
+    
+    for(int i=0; i<money.size(); i++){
+        for(int j=money[i]; j<=n; j++){
+            dp[j]+=dp[j-money[i]];
+            dp[j] %= 1000000007;
+        }
+    }
+    return dp[n];
+}
+
+int solution(int n, vector<int> money) {
+    int answer = dynamic(n,money);
+    return answer;
+}
