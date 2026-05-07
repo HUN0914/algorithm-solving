7cd786876f75d0946245630d2a6f4f31e9400d20
Gwanghun Baek <74861510+HUN0914@users.noreply.github.com>
2026-05-08 06:11:25 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,2 @@
+select fi.ID, fni.FISH_NAME, fi.LENGTH from FISH_INFO fi join FISH_NAME_INFO fni on fi.FISH_TYPE=fni.FISH_TYPE
+where (fi.FISH_TYPE, fi.LENGTH) in (select FISH_TYPE, max(LENGTH) from FISH_INFO group by FISH_TYPE);
